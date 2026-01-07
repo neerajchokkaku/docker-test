4f06be8cb330b73d60efb12489e9579506784471
Chokkaku Thanooj <chokkakuthanooj@Chokkakus-MacBook-Air.local>
2026-01-07 12:48:34 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ if __name__ == '__main__':
     host = '127.0.0.1'
     port = 5000
     print(f"Starting server at http://{host}:{port}")
+    print(f"Starting server at http://{host}:{port}")
     app.run(host=host, port=port, debug=True)
